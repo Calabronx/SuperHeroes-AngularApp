ec5edc2756e2765bc50b7a74a45a3c299e9bafd5
Calabronx <69681105+Calabronx@users.noreply.github.com>
2022-11-17 18:01:22 -0300

    8 files modified, 5 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Servicios
+
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroesService } from './services/heroes.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, APP_ROUTING],
   declarations: [
     AppComponent,
     NavbarComponent,
@@ -17,6 +24,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     AboutComponent,
     HeroesComponent,
   ],
+  providers: [HeroesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
