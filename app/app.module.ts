ccf3dad4437cbd8bfac0c70e72b599aa86abcecb
Calabronx <69681105+Calabronx@users.noreply.github.com>
2022-11-17 11:21:25 -0300

    4 files modified, 4 added, 2 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,10 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
